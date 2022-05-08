21dfb709e4f80a4a307138ef6b7d907f683be167
Nelio Alves <acenelio@gmail.com>
2022-05-08 05:50:15 -0300

    empregados

---
+++ empregados.sql
@@ -0,0 +1,22 @@
+CREATE TABLE tb_departamento (
+    id int8, 
+    nome varchar(100), 
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE tb_empregado (
+    id int8, 
+    nome varchar(100), 
+    dept_id int8,
+    PRIMARY KEY (id),
+    FOREIGN KEY (dept_id) REFERENCES tb_departamento
+);
+
+INSERT INTO tb_departamento (id, nome) VALUES (1, 'Financeiro');
+INSERT INTO tb_departamento (id, nome) VALUES (2, 'Recursos Humanos');
+INSERT INTO tb_departamento (id, nome) VALUES (3, 'Comercial');
+
+INSERT INTO tb_empregado (id, nome, dept_id) VALUES (1, 'Maria', 1);
+INSERT INTO tb_empregado (id, nome, dept_id) VALUES (2, 'João', 2);
+INSERT INTO tb_empregado (id, nome, dept_id) VALUES (3, 'Ana', 1);
+INSERT INTO tb_empregado (id, nome, dept_id) VALUES (4, 'Carlos', 2);
